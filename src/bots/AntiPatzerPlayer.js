de0f4d72c63ce5b0474607bf5cf8640c4406a8d0
N1K0LAAAA <114262292+N1K0LAAAA@users.noreply.github.com>
2023-08-20 03:55:55 +0200

    Add files via upload

--- src/bots/AntiPatzerPlayer.js
+++ src/bots/AntiPatzerPlayer.js
@@ -1,48 +1,105 @@
-const ChessUtils = require("../utils/ChessUtils")
+const ChessUtils = require("../utils/ChessUtils");
+const getBoardEval = require("../utils/EvalFunction");
 
-/**
- * Do not play moves that leave opponent with mate in one or checks or captures.
- * else random.
- *
- */
 class AntiPatzerPlayer {
-  getNextMove(moves) {
-    const chess = new ChessUtils()
-    chess.applyMoves(moves)
-    const legalMoves = chess.legalMoves()
+  // ... existing code ...
+  positionCount = 0;
 
-    if (legalMoves.length) {
-      legalMoves.forEach((m) => {
-        chess.move(m)
-        const opponentsMoves = chess.legalMoves()
-        const opponentsMates = opponentsMoves.filter((move) =>
-          /#/.test(move.san)
-        )
-        const opponentsChecks = opponentsMoves.filter((move) =>
-          /\+/.test(move.san)
-        )
-        const opponentsCaptures = opponentsMoves.filter((move) =>
-          /x/.test(move.san)
-        )
-        m.metric = -opponentsMoves.length
-        m.metric += -opponentsCaptures.length * 10
-        m.metric += -opponentsChecks.length * 100
-        m.metric += -opponentsMates.length * 1000
-        chess.undo()
-      })
+  minimaxRoot(depth, chess, isMaximisingPlayer) {
+    let newGameMoves = chess.legalMoves();
+    let bestMove = (isMaximisingPlayer) ? -Infinity : Infinity; 
+    let bestMoveFound;
 
-      // choose move that maximises metric
-      return chess.uci(legalMoves.reduce(this.randomMax))
+    for (var i = 0; i < newGameMoves.length; i++) {
+      let newGameMove = newGameMoves[i]
+      chess.move(newGameMove);
+      let value = this.minimax(depth - 1, chess, -Infinity, Infinity, !isMaximisingPlayer);
+      chess.undo();
+      if ((isMaximisingPlayer) ? bestMove <= value : bestMove >= value) {
+        bestMove = value;
+        bestMoveFound = newGameMove;
+      }
+    }
+    return bestMoveFound;
+  }
+
+  minimax(depth, chess, alpha, beta, isMaximisingPlayer) {
+    this.positionCount++;
+   // const chess = new ChessUtils();
+    // chess.chess.load(chessFen);
+    let cords = chess.getAllSquares();
+
+    if (depth === 0) return   getBoardEval(cords);
+
+
+    const legalMoves = chess.legalMoves();
+    if (isMaximisingPlayer) {
+      let bestMove = -Infinity;
+      for (const move of legalMoves) {
+        chess.move(move);
+        bestMove = Math.max(bestMove, this.minimax(depth - 1, chess, alpha, beta, !isMaximisingPlayer));
+        chess.undo();
+        alpha = Math.max(alpha, bestMove);
+        if (beta <= alpha) {
+          // Beta cutoff
+          break;
+        }
+      }
+      return bestMove;
+    } else {
+      let bestMove = Infinity;
+      for (const move of legalMoves) {
+        chess.move(move);
+        bestMove = Math.min(bestMove, this.minimax(depth - 1, chess, alpha, beta, !isMaximisingPlayer));
+        chess.undo();
+        beta = Math.min(beta, bestMove);
+        if (beta <= alpha) {
+          // Alpha cutoff
+          break;
+        }
+      }
+      return bestMove;
     }
   }
 
-  randomMax(a, b) {
-    return a.metric + Math.random() > b.metric + Math.random() ? a : b
+  getNextMove(moves) {
+    const chess = new ChessUtils();
+    chess.applyMoves(moves);
+    const legalMoves = chess.legalMoves();
+    const who = chess.turn();
+    const bestMove = this.findBestMove(chess, legalMoves, who);
+    return bestMove;
+  }
+
+  findBestMove(chess, legalMoves, who) {
+    let depth = 2; // Adjust the depth as per your requirement
+    const isMaximisingPlayer = who === "w";
+    this.positionCount = 0;
+    var d = new Date().getTime();
+    let bestMove;
+    while(this.positionCount < 3500 && depth < 10){
+       this.positionCount = 0;
+       bestMove = this.minimaxRoot(++depth, chess, isMaximisingPlayer);
+    }
+    const bestMoveString = chess.uci(bestMove);
+    console.log(bestMoveString)
+    var d2 = new Date().getTime();
+    var moveTime = (d2 - d) / 1000;
+    var positionsPerS = ( this.positionCount / moveTime);
+    console.log("Possible Moves: " + chess.legalMoves().length);
+    console.log("Positions Counted: " + this.positionCount);
+    console.log("Calculation Time: " + moveTime);
+    console.log("Position/s: " + positionsPerS);
+    console.log("Depth: " + depth);
+    chess.move(bestMove);
+    console.log("Eval: " + getBoardEval(chess.getAllSquares()));
+    chess.undo();
+    return bestMoveString;
   }
 
   getReply(chat) {
-    return "AntiPatzerPlayer"
+    return "Hello";
   }
 }
 
-module.exports = AntiPatzerPlayer
+module.exports = AntiPatzerPlayer;
